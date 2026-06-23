3673f5c62fd89f4086b0ed2667467b69148be02a
Mathieu Pineau <mathieu.pineau@zenika.com>
2026-06-23 10:24:52 +0200

    Init Spring project

---
+++ src/main/java/com/example/formation_ddd_zenika/FormationDddZenikaApplication.java
@@ -0,0 +1,13 @@
+package com.example.formation_ddd_zenika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormationDddZenikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormationDddZenikaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/formation_ddd_zenika/FormationDddZenikaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.formation_ddd_zenika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormationDddZenikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
